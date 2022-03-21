aeb1e5f29223a5d8670de2adb11a801765f87735
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2022-03-21 12:22:12 -0500

    No more functions. We have numpy one-liners. And str.format().

--- primes_numpy.py
+++ primes_numpy.py
@@ -2,17 +2,17 @@
 https://oeis.org/A038822
 Number of primes between 100n and 100n+99.
 Look at the scatterplot graph. < 5 primes in a 100 digit range is pretty special.
-Exepect the first primefree century occurs at 16718 (1671800  .. 1671899).
+Expect the first primefree century occurs at 16718 (1671800  .. 1671899).
+
+For sequence of primes per century:
+EXPECTED = np.array([25, 21, 16, 16, 17, 14, 16, 14, 15, 14, 16, 12, 15, 11, 17, 12, 15, 12, 12, 13, 14, 10])
 """
 
 import numpy as np
-
-EXPECTED = np.array([25, 21, 16, 16, 17, 14, 16, 14, 15, 14, 16, 12, 15, 11, 17, 12, 15, 12, 12, 13, 14, 10])
-
-MAX_NUM = 10 ** 7  # 100 * len(EXPECTED)
+MAX_NUM = 6100000
 
 
-def sievefrom2to(n):
+def primesfrom2to(n):
     # https://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n-in-python/3035188#3035188
     """ Input n>=6, Returns a array of primes, 2 <= p < n """
     sieve = np.ones(n // 3 + (n % 6 == 2), dtype=np.bool)
@@ -22,36 +22,16 @@ def sievefrom2to(n):
             k = 3 * i + 1 | 1
             sieve[((k * k) // 3)::2 * k] = False
             sieve[(k * k + 4 * k - 2 * k * (i & 1)) // 3::2 * k] = False
-    return sieve
-
-
-def primesfrom2to(n):
-    sieve = sievefrom2to(n)
     return np.r_[2, 3, ((3 * np.nonzero(sieve)[0] + 1) | 1)]
 
 
-def sparse_centuries_primes(p, max_primes=1):
-    """Basically a printing function, very little math."""
-    list_of_str = []
-    count_list = primes_per_century_vec(p)
-    for century, count in enumerate(count_list):
-        lower = century * 100
-        upper = lower + 99
-        if count <= max_primes:
-            list_of_str.append("%i %i %i %s" % (century, lower, upper, count))
-    return list_of_str
-
-
-def primes_per_century_vec(p):
-    return np.bincount(p // 100)
-
-
 if __name__ == '__main__':
-    # print('\n'.join(sparse_centuries_primes(P, 100)))
-    # print()
-    P = primesfrom2to(MAX_NUM)
+    p = primesfrom2to(MAX_NUM)
+    primes_per_century = np.bincount(p // 100)
 
-    #simple_diff = primes_per_century_vec(P)
-    #print(simple_diff)
+    print("Primefree centuries below {:,}:".format(MAX_NUM))
+    print(np.flatnonzero(primes_per_century == 0))
+    print()
 
-    print('\n'.join(sparse_centuries_primes(P)))
+    print("Single-prime centuries below {:,}:".format(MAX_NUM))
+    print(np.flatnonzero(primes_per_century == 1))
